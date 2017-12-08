05358637e7f38e6ffdfd4a1fadb0fcfdcbefb853
Akshay Kanthi <aksanoble@gmail.com>
2017-12-09 01:57:52 +0530

    assembler! Needs refactor

--- 06/Assembler/assembler.js
+++ 06/Assembler/assembler.js
@@ -1,18 +1,61 @@
 const fs = require("fs");
-const inputASM = fs.readFileSync("../max/MaxL.asm");
+const inputASM = fs.readFileSync("../pong/PongL.asm");
 const inputList = inputASM.toString().split(/\r\n|\n/);
 const fileName = process.argv[2].split(".asm")[0];
 const symbolTable = require("./spec").symbolTable;
 const spec = require("./spec");
 const writeFileName = `${fileName}.hack`;
-// const machineInstructions = [];
-
+// var symbols = {};
 const stripInput = inputList => {
   return inputList
     .map(str => str.replace(/\/\/.*/, "").trim())
     .filter(str => !str == "");
 };
-const machineInstructions = stripInput(inputList).map(asm => {
+
+const firstPass = (instructions) => {
+  let iCount = 0;
+  var filtered = instructions.filter(asm => {
+    if (asm.includes("(")) {
+      const location = asm.replace(/\(|\)/g, "");
+      console.log(location, "asm", asm);
+      symbolTable[location] = iCount;
+      return false;
+    }
+    iCount++;
+    return true;
+  });
+
+  return filtered;
+};
+
+const secondPass = (instructions) => {
+  let memory = 15;
+  return instructions.map(i => {
+    if (i.includes('@')) {
+      const sym = i.split('@')[1]
+      if (Number.isInteger(parseInt(sym))) {
+        return `@${sym}`
+      }
+      if (symbolTable[sym] === undefined) {
+        symbolTable[sym] = ++memory
+      }
+
+      return `@${symbolTable[sym]}`
+
+    } return i
+  })
+
+}
+
+// console.log(secondPass(firstPass(stripInput(inputList))))
+// console.log(firstPass(stripInput(inputList)));
+// console.log(symbolTable, "sym");
+
+const instructions = secondPass(firstPass(stripInput(inputList)));
+
+console.log(instructions, 'instructions');
+
+const machineInstructions = instructions.map(asm => {
   const inst = {
       comp: "",
       dest: "",
@@ -38,7 +81,7 @@ const machineInstructions = stripInput(inputList).map(asm => {
     inst.jmp = splitJmp[1];
   }
 
-  console.log(inst, 'inst')
+  // console.log(inst, 'inst')
     return `111${spec.opCodes[inst.comp]}${spec.destCodes[inst.dest]}${spec
       .jmpCodes[inst.jmp]}`;
 });
